ebf32e20513af21c761097a700192ce8afde86fe
SavasUOzdemir <94976406+SavasUOzdemir@users.noreply.github.com>
2023-04-29 01:30:43 +0300

    Create scenes, add some functionality to splashscreen

---
+++ FISHFORMERS/Assets/CanvasFunctions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasFunctions : MonoBehaviour
+{
+    OptionsCanvas optsCanvas;
+    private void Awake()
+    {
+        optsCanvas=FindObjectOfType<OptionsCanvas>(includeInactive:true);
+    }
+    public void StartButtonPress()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OptionsButtonPress()
+    {
+        optsCanvas.gameObject.SetActive(true);
+    }
+}
---
+++ FISHFORMERS/Assets/OptionsCanvas.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsCanvas : MonoBehaviour
+{
+    //Static Object Instance//
+    public static OptionsCanvas Instance { get; private set; }
+    //Slider Object References//
+    [SerializeField] Slider musicVolumeSlider;
+    [SerializeField] Slider sfxVolumeSlider;
+    public delegate void OnVolumeChangeDelegate();
+    public event OnVolumeChangeDelegate OnVolumeChange;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void XButtonPressed()
+    {
+        gameObject.SetActive(false);
+    }
+    public void SavePrefs()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolumeSlider.value);
+        OnVolumeChange();
+    }
+}
