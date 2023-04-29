a21abd7d215ba73bfc1c580b22d0b3f7a0d68ffe
SavasUOzdemir <94976406+SavasUOzdemir@users.noreply.github.com>
2023-04-29 15:33:17 +0300

    add small method to orient spriterenderer properly

--- FISHFORMERS/Assets/Scripts/PlayerController.cs
+++ FISHFORMERS/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
     Vector2 movement;
 
     private void Awake()
     {
-        rb2d = GetComponentInChildren<Rigidbody2D>(includeInactive:false);
+        rb2d = GetComponentInChildren<Rigidbody2D>(includeInactive: false);
+        sr = GetComponentInChildren<SpriteRenderer>(includeInactive: false);
     }
 
     private void FixedUpdate()
@@ -27,8 +29,15 @@ public class PlayerController : MonoBehaviour
         // Move player
         movement = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
         if (movement != Vector2.zero)
-        {
             transform.right = movement / moveSpeed;
-        }
+        OrientSpriteRenderer();
+    }
+
+    void OrientSpriteRenderer()
+    {
+        if ((transform.rotation.eulerAngles.z >= 135 && transform.rotation.eulerAngles.z < 180) || (transform.rotation.eulerAngles.z > 180 && transform.rotation.eulerAngles.z <= 225))
+            sr.flipY = true;
+        else sr.flipY = false;
     }
 }
+
