1b71ccfe6b029f2a7eedba10810a3f8af989bcdf
SavasUOzdemir <94976406+SavasUOzdemir@users.noreply.github.com>
2023-04-29 04:22:27 +0300

    Add cinemachine virtual camera
    and a placeholder background for testing the camera

--- FISHFORMERS/Assets/Scripts/PlayerController.cs
+++ FISHFORMERS/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,19 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb2d;
+    Vector2 movement;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        //rb2d.AddForce(movement);
+        rb2d.velocity = movement;
+
+    }
     private void Update()
     {
         // Get input from keyboard
@@ -20,11 +27,10 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         // Move player
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
-        if (movement!=Vector2.zero)
+        movement = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
+        if (movement != Vector2.zero)
         {
             transform.right = movement / moveSpeed;
         }
-        rb2d.velocity = movement;
     }
 }
