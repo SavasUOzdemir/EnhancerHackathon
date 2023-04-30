0f8db6cc076f5d179eb0a472e9a47e40d1df7f1f
SavasUOzdemir <94976406+SavasUOzdemir@users.noreply.github.com>
2023-04-30 07:31:02 +0300

    add breakableObject

---
+++ FISHFORMERS/Assets/BreakableObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    [SerializeField]int hitPoints = 3;
+    SpriteRenderer spriteRenderer;
+    int color_g = 255;
+    int color_b = 255;
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            Debug.Log("Hit!");
+            hitPoints--;
+            if (hitPoints == 0)
+            { Destroy(gameObject); return; }
+            color_b -= 255 / hitPoints;
+            color_g -= 255 / hitPoints;
+            spriteRenderer.color = new Color(255, color_g, color_b);
+        }
+    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Weapon"))
+    //    {
+    //        Debug.Log("Hit!");
+    //        hitPoints--;
+    //        if (hitPoints == 0)
+    //            Destroy(gameObject);
+    //        color_b -= 255/hitPoints;
+    //        color_g -= 255/hitPoints;
+    //        spriteRenderer.color = new Color(255, color_g, color_b);
+    //    }
+        
+    
+}
